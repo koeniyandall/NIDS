bf03a761f05475f7d10bbe64d24c128c0bbc0d56
koeniyandall <126587319+koeniyandall@users.noreply.github.com>
2025-09-04 16:22:46 -0400

    made rulset and IDS code

--- src/nids.cpp
+++ src/nids.cpp
@@ -5,6 +5,19 @@
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+
+struct Rule {
+	std::string protocol;   // "tcp", "udp", etc.
+	int dst_port;           // destination port (or -1 for any)
+	std::string content;    // string to search in payload
+	std::string msg;        // alert message
+};
+
+std::vector<Rule> rules;  // actual definition (not extern)
 
 // Defines the libpcap API (pcap_loop, etc.)
 // Defines standard I/O streams (std::cout, std::cerr)
@@ -15,13 +28,46 @@
 // Defines inet_ntop and byte-order helper for addresses
 
 // Define constants
-#define ETHER_HDR_LEN 14
 #define SNAP_LEN 65536
 
 // A user data struct to pass to the handler
 struct PacketHandlerData {
 	int packet_count = 0;
 };
+
+
+// Example: alert tcp any any -> any 80 (content:"UNION SELECT"; msg:"SQLi")
+std::vector<Rule> read_rules(const std::string filename){
+	std::vector<Rule> rules;
+	std::ifstream file(filename);
+	std::string line;
+	while(getline(file, line)){
+			if (line.empty() || line[0] == '#') continue;
+			std::string action,proto,src,arrow,dst;
+			Rule rule;
+			std::istringstream iss(line);
+			iss >> action >> proto >> src >> src >> arrow >> dst >> rule.dst_port;
+			rule.protocol = proto;
+			size_t start = line.find('(');
+			size_t end = line.find(')');
+			if (start != std::string::npos && end != std::string::npos) {
+			std::string opts = line.substr(start + 1, end - start - 1);
+			if (opts.find("content:") != std::string::npos) {
+			     size_t cstart = opts.find("content:\"") + 9;
+			     size_t cend = opts.find("\"", cstart);
+			     rule.content = opts.substr(cstart, cend - cstart);
+			}
+			if (opts.find("msg:") != std::string::npos) {
+			     size_t mstart = opts.find("msg:\"") + 5;
+			     size_t mend = opts.find("\"", mstart);
+			     rule.msg = opts.substr(mstart, mend - mstart);
+			}
+			}
+			rules.push_back(rule);
+	}
+	return rules;
+}
+
 
 // Sets the Ethernet header to start at the beginning of the packet
 // const is important here to signify that this function will not change the packet
@@ -77,10 +123,21 @@ void packet_handler(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char
 		<< " | Destination Port: " << ntohs(tcp_header->th_dport) << std::endl;
 
 	//setting null terminator and printing the payload
-	for(int i = 0; i < payload_length;  i++){
+	/*for(int i = 0; i < payload_length;  i++){
 		std::cout << payload[i];
 	}
-}
+	*/
+	extern std::vector<Rule> rules; // declare rules globally
+	std::string payload_str(reinterpret_cast<const char*>(payload), payload_length);
+	for(auto &rule: rules){
+		if(rule.protocol == "tcp"){
+			if(rule.dst_port == ntohs(tcp_header->th_dport)){
+				if(payload_str.find(rule.content) != std::string::npos){
+					std::cout << "[Alert] " << rule.content << " | src: " << source_ip << " : " << ntohs(tcp_header->th_sport) << " dst: " << dest_ip << " : " << ntohs(tcp_header->th_dport) << std::endl; 
+				}
+	}
+
+		}}}
 
 
 int main(int argc, char *argv[]) {
@@ -93,6 +150,8 @@ int main(int argc, char *argv[]) {
 	if (argc > 1) {
 		interface = argv[1];
 	}
+
+	std::vector<Rule> rules = read_rules("rules.txt");
 
 	// Open the network interface for live capture
 	handle = pcap_open_live(interface, SNAP_LEN, 1, 1000, errbuf);
